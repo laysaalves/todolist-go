56e0d4655fa31260654a082ae4fcab4ff03b0df5
Laysa <laysa.developer@gmail.com>
2024-12-15 12:35:13 -0300

    feat: add initial gin api structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+  "net/http"
+
+  "github.com/gin-gonic/gin"
 )
 
-func main(){
-	fmt.Println("ola mundo")
+func main() {
+  r := gin.Default()
+  r.GET("/ping", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+  })
+  r.Run()
 }
